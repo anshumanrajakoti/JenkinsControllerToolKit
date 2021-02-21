47ae56cb7eb9ebe0378dd4258a301d018378eac0
arajakoti <a.r@g.com>
2021-02-20 22:29:48 -0500

    fascade pattern and controller method declaration

--- src/com/fs/jctk/Controller.java
+++ src/com/fs/jctk/Controller.java
@@ -1,5 +1,50 @@
 package com.fs.jctk;
 
-public class Controller {
+public class Controller extends Trigger{
+	
+	public static String jenkinsConnector(String apiMethod, String jenkinsURL) {
+		
+		String connectionStatus = "Failed";
+				
+		return connectionStatus;
+		
+	}
+	
+	public static void disconnectJenkins(){
+		
+		/*
+		connection.disconnect();
+		connection = null;
+		*/
+	}
+	
+	public static String triggerJenkinsBuild(){
+		
+		String connectionStatus = "";
+		connectionStatus = jenkinsConnector("POST", "http://localhost:8080");
+		
+		return connectionStatus;
+		
+	}
+	
+	public static String getJenkinsProgress(){
+		
+		getJenkinsBuildNumber();
+		return verifyBuildStatus();
+		
+	}
+	
+	public static int getJenkinsBuildNumber(){
+		
+		return buildNumber;
+		
+	}
+	
+	public static String verifyBuildStatus(String ...buildNumber){
+		
+		String jobStatus = "";
+		return jobStatus;
+		
+	}
 
 }
--- src/com/fs/jctk/Trigger.java
+++ src/com/fs/jctk/Trigger.java
@@ -1,5 +1,18 @@
 package com.fs.jctk;
 
 public class Trigger {
+	
+	public static int buildNumber = 1;
 
+	public static void main( String[] args){
+		
+		System.out.println("Hello World..!");
+		
+	}
+	
+	public static String callJenkins(String jenkinsJObURL, String userName, String apiJenkinsToken, int depthOfLog){
+		
+		String logExtract = "";
+		return logExtract;
+	}
 }
